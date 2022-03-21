9af671b3e8c48b0815779becc267611aa42ffdc5
Richie Cahill <richie@tmmworkshop.com>
2022-03-21 00:12:01 -0400

    got count funtion working in fast_eratosthenes_sieve

--- fast_eratosthenes_sieve/src/main.rs
+++ fast_eratosthenes_sieve/src/main.rs
@@ -3,17 +3,19 @@ use num::integer::sqrt;
 // use std::arch::x86_64::*;
 
 // counts the number prime numbers in the array minus the bits that are
-// fn count(_prime: Vec<u64>, _size: usize, _extra: usize) -> usize{
-// 	let temp = 0;
-// 		for i in 0.._size {
-// 			temp += 64-_popcnt64;(_prime[i]);
-// 		}
-			
-// 		return temp-_mm_popcnt_u64;(_prime[_size-1]>>(64-_extra));
-// }
+fn count(_prime: &Vec<u64>, _size: usize, _extra: u32) -> u32{
+	let mut temp = 0;
+		for i in 0.._size {
+				temp += _prime[i].count_zeros();
+		}
+		// return temp - (extra-_mm_popcnt_u64(prime[size-1]>>(64-extra)));
+
+		return temp	- (_extra-(_prime[_size-1]>>(64-_extra)).count_ones())
+		;
+}
 	
 // fn eratosthenes_sieve(_n: usize, _prime: Vec<u64>, _size: usize) -> (){
-fn eratosthenes_sieve(_n: usize,mut _prime: Vec<u64>) -> (){
+fn eratosthenes_sieve(_n: usize,_prime: &mut Vec<u64>) -> (){
 	for _i in (3..sqrt(_n)).step_by(2) {
 		if (_prime[_i>>7] & (1<<((_i>>1)%64))) == 0{
 			for _j in (_i*_i.._n).step_by(2*_i) {
@@ -32,7 +34,7 @@ fn eratosthenes_sieve(_n: usize,mut _prime: Vec<u64>) -> (){
 
 fn main() {
 	// The number you want to calculate to
-	const TOTAL: usize = 1000;
+	const TOTAL: usize = 1024;
 	// calculates next multipule of 128 above Total
 	const MULT: usize =128-(TOTAL%128)+TOTAL;
 	// calculates the difference  between total nad Mult
@@ -40,15 +42,14 @@ fn main() {
 	// create and list of 64bit ints 128 time smaller then Mult
 	const SIZE: usize  = MULT/128;
 	let mut _prime: Vec<u64> = vec![0; SIZE];
-
 	// eratosthenes_sieve(MULT,_prime,SIZE);
-	eratosthenes_sieve(MULT,_prime);
+	eratosthenes_sieve(MULT,&mut _prime);
 
 
 
-	// let test = count(_prime, SIZE, EXTRA);
+	let test = count(&_prime, SIZE, EXTRA.try_into().unwrap());
 
-	// println!("{}", test);
+	println!("{}", test);
 
 
 
