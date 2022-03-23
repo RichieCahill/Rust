268312f27db67300c6aac8c77cd9a448f0f14264
Richie Cahill <richie@tmmworkshop.com>
2022-03-23 15:14:08 -0400

    added a speed test to fast_eratosthenes_sieve and make fast_128_eratosthenes_sieve

--- fast_128_eratosthenes_sieve/src/main.rs
+++ fast_128_eratosthenes_sieve/src/main.rs
@@ -12,23 +12,29 @@ fn _popcntu64(mut x: u64) -> u64{
 	}
 }
 
+fn _popcntu128(n: u128) -> u64{
+	_popcntu64((n & 0x0000000000000000FFFFFFFFFFFFFFFF).try_into().unwrap())+_popcntu64((n >> 64).try_into().unwrap())
+}
+
+
+
 // counts the number prime numbers in the array minus the bits that are
-fn count(_prime: &Vec<u64>, _size: usize, mut _extra: usize) -> u64{
+fn count(_prime: &Vec<u128>, _size: usize, mut _extra: usize) -> u64{
 	let mut temp = 0;
 		for i in 0.._size {
-				temp += 64-_popcntu64(_prime[i]);
+				temp += 128-_popcntu128(_prime[i]);
 		}
 				let _extra: u64 = _extra.try_into().unwrap();
-				return temp -(_extra-_popcntu64(_prime[_size-1]>>(64-_extra)));
+				return temp -(_extra-_popcntu128(_prime[_size-1]>>(128-_extra)));
 }
 	
-// fn eratosthenes_sieve(_n: usize, _prime: Vec<u64>, _size: usize) -> (){
-fn eratosthenes_sieve(_n: usize,_prime: &mut Vec<u64>) -> (){
+// fn eratosthenes_sieve(_n: usize, _prime: Vec<u128>, _size: usize) -> (){
+fn eratosthenes_sieve(_n: usize,_prime: &mut Vec<u128>) -> (){
 	use num::integer::sqrt;
 	for _i in (3..sqrt(_n)).step_by(2) {
-		if (_prime[_i>>7] & (1<<((_i>>1)%64))) == 0{
+		if (_prime[_i/256] & (1<<((_i>>1)%128))) == 0{
 			for _j in (_i*_i.._n).step_by(2*_i) {
-				_prime[_j>>7] |= 1<< ((_j>>1)%64);
+				_prime[_j/256] |= 1<< ((_j>>1)%128);
 			}
 		}
 	}
@@ -40,12 +46,12 @@ fn main() {
 	// The number you want to calculate to
 	const TOTAL: usize = 10000000000;
 	// calculates next multipule of 128 above Total
-	const MULT: usize =128-(TOTAL%128)+TOTAL;
+	const MULT: usize =256-(TOTAL%256)+TOTAL;
 	// calculates the difference  between total nad Mult
 	const EXTRA: usize  = (MULT-TOTAL)/2;
 	// create and list of 64bit ints 128 time smaller then Mult
-	const SIZE: usize  = MULT/128;
-	let mut _prime: Vec<u64> = vec![0; SIZE];
+	const SIZE: usize  = MULT/256;
+	let mut _prime: Vec<u128> = vec![0; SIZE];
 
 	use std::time::Instant;
 	let now = Instant::now();
@@ -57,5 +63,4 @@ fn main() {
 
 	let elapsed = now.elapsed();
 	println!("Elapsed: {:.2?}", elapsed);
-
 }
