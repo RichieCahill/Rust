694e0d8de327ae2f4dc7822a88ed12b2091b050c
Richie Cahill <richie@tmmworkshop.com>
2022-04-05 21:50:14 -0400

    attempted to optimize fast_eratosthenes_sieve with david

--- fast_eratosthenes_sieve/src/main.rs
+++ fast_eratosthenes_sieve/src/main.rs
@@ -1,3 +1,4 @@
+//David Krauthamer https://gitlab.com/Silent__Ninja346 Helped optomize this
 
 // _mm_popcnt_u64 For rust
 #[inline]
@@ -22,13 +23,25 @@ fn count(prime: &Vec<u64>, size: usize, extra: usize) -> u64{
 				return temp -(extra-_popcntu64(prime[size-1]>>(64-extra)));
 }
 	
+
+fn is_prime(prime: &Vec<u64>, pos: usize) -> bool{
+	unsafe {
+		*prime.get_unchecked(pos>>7) & (1<<((pos>>1)%64)) == 0
+	}
+}
+
+fn clear_prime(prime: &mut Vec<u64>, pos: usize) {
+	unsafe {
+		*prime.get_unchecked_mut(pos>>7) |= 1<< ((pos>>1)%64)
+	}
+}
+
 // fn eratosthenes_sieve(n: usize, prime: Vec<u64>, size: usize) -> (){
 fn eratosthenes_sieve(n: usize,prime: &mut Vec<u64>) -> (){
-	use num::integer::sqrt;
-	for i in (3..sqrt(n)).step_by(2) {
-		if (prime[i>>7] & (1<<((i>>1)%64))) == 0{
+	for i in (3..((n as f32).sqrt()as usize)).step_by(2) {
+		if is_prime(& prime,i){
 			for j in (i*i..n).step_by(2*i) {
-				prime[j>>7] |= 1<< ((j>>1)%64);
+				clear_prime(prime,j);
 			}
 		}
 	}
@@ -38,7 +51,7 @@ fn eratosthenes_sieve(n: usize,prime: &mut Vec<u64>) -> (){
 
 fn main() {
 	// The number you want to calculate to
-	const TOTAL: usize = 1000000000;
+	const TOTAL: usize = 10000000000;
 	// calculates next multipule of 128 above Total
 	const MULT: usize =128-(TOTAL%128)+TOTAL;
 	// calculates the difference  between total nad Mult
@@ -50,8 +63,12 @@ fn main() {
 	use std::time::Instant;
 	let now = Instant::now();
 
-	// eratosthenes_sieve(MULT,prime,SIZE);
 	eratosthenes_sieve(MULT,&mut prime);
+
+	// let elapsed = now.elapsed();
+	// println!("Elapsed: {:.2?}", elapsed);
+
+	// let now = Instant::now();
 
 	println!("{}", count(&prime, SIZE, EXTRA.try_into().unwrap()));
 
