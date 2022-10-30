f30778fc3aa652ce2dd7a2599d54f91afc0f4ae5
Richie Cahill <richie@tmmworkshop.com>
2022-10-30 08:45:25 -0400

    Spoke with dave and rewrote import_list

--- Networking/server/src/main.rs
+++ Networking/server/src/main.rs
@@ -1,27 +1,33 @@
 // use std::io::prelude::*;
-// use std::str::{FromStr};
-use std::io::{self, Read, Write};
-use std::net::{TcpListener,TcpStream,Shutdown};
-use std::hash::{Hash,Hasher};
-use std::collections::hash_map::{self, DefaultHasher};
-use std::fs;
+use std::{
+	collections::hash_map::{self, DefaultHasher},
+	fs,
+	hash::{Hash,Hasher},
+	io::{self, Read, Write},
+	net::{TcpListener,TcpStream,Shutdown},
+	path::{PathBuf},
+	str::{FromStr},
+	string::ToString
+};
 
-fn spawner (ip: &str, list: &Vec<&str>,mut pos: usize) -> io::Result<()> {
+fn spawner (ip: &str, list: Vec<String>,mut pos: usize) -> io::Result<()> {
 	let mut stream = TcpStream::connect(ip)?;
-	let hash = calculate_hash(list[pos]).to_string();
-	let spos = pos.to_string();
-	let buf = spos+&hash+list[pos];
-	stream.write(buf.as_bytes())?;
+	let hash = calculate_hash(list[pos].clone()).to_string();
+	let buf = pos.to_string()+&hash+&list[pos];
+	if buf.len() < 150 {
+		stream.write_all(buf.as_bytes())?;
+	} else {
+		println!("error buf To long");
+	}
 
-	let mut rx_bytes = String::new();
-	let _test=stream.read_to_string(&mut rx_bytes);
-	let num = rx_bytes.parse::<i32>().unwrap();
-	// let mut rx_bytes = [0u8; 1];
-	// let received = std::str::from_utf8(&rx_bytes).expect("valid utf8");
-	// let num = received.parse::<i32>().unwrap();
+	let mut rx_bytes = [0u8; 1];
+	let _test=stream.read(&mut rx_bytes);
+	let received = std::str::from_utf8(&rx_bytes).expect("valid utf8");
+	println!("test");
+	let num = received.parse::<i32>().unwrap();
 
 	println!("{}", num);
-
+	
 	if num == 1 {
 		print!("yay");
 		pos+=1;
@@ -37,28 +43,31 @@ I hate this function with a burning passion!!! However it should work.
 The problems are as its 2 loop isn't the end of the world because the current dataset is relatively small.
 I think it should be possible to make it a single loop to read and clean the data.
 Also its copying the vec I was just fed up fighting with the borrow checker.
+// https://doc.rust-lang.org/stable/std/path/struct.PathBuf.html
 */
-fn import_list (file_path: &str,mut out: &Vec<&str>){
-	let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
-	let mut list = contents.lines().collect::<Vec<_>>();
-	for temp in list.iter_mut() {
-		*temp = temp.strip_prefix("/ZFS/Storage/Plex/").unwrap();
-	}
-	let mut out = list.to_owned();
-	// out = &list.to_owned();
+fn import_list(file_path: PathBuf) -> Vec<String> {
+	let contents = fs::read_to_string(file_path);
+	let list: Vec<String> = contents.expect("REASON").lines()
+			.filter_map(|val| match val.strip_prefix("/ZFS/Storage/Plex/") {
+					Some(v) => Some(v),
+					None => {
+							println!("A fucky wucky happened parsing !");
+							None
+					}
+			})
+			.map(String::from_str).map(Result::unwrap).collect();
+	list
 }
 
 /*
 Taken for https://doc.rust-lang.org/std/hash/index.html#
 Just hashes a string
 */
-fn calculate_hash(string: &str) -> u64 {
+fn calculate_hash(string: String) -> u64 {
 	let mut temp = DefaultHasher::new();
 	string.hash(&mut temp);
 	temp.finish()
 }
-
-
 
 fn request_handler(mut stream: &TcpStream) -> io::Result<()> {
 	// Array with a fixed size
@@ -80,20 +89,17 @@ stream.flush()
 
 
 fn main() {
-	// let mut list: Vec<&str> = vec![];
+	let mut list: Vec<String> = vec![];
 	let ips = vec!["127.0.0.1:8080"];
 	const FILE_PATH: &str = "/home/r2r0m0c0/Projects/Rust/Networking/server/list.txt";
 	let mut pos = 0;
 
-	// import_list(&FILE_PATH,&list);
-	let contents = fs::read_to_string(FILE_PATH).expect("Should have been able to read the file");
-	let mut list = contents.lines().collect::<Vec<_>>();
-	for temp in list.iter_mut() {
-		*temp = temp.strip_prefix("/ZFS/Storage/Plex/").unwrap();
-	}
-	// for ip in ips {
-		spawner (ips[0],&list,pos);
+	list = import_list((&FILE_PATH).into());
+	// for ip in &ips {
+		spawner (ips[0],list,pos);
+		// pos+=1;
 	// }
+
 	let listener = TcpListener::bind("127.0.0.1:8081").unwrap();
 	for stream in listener.incoming() {
 		match stream {
@@ -105,3 +111,5 @@ fn main() {
 	}
 }
 
+// clippy
+// rist fmt
